b673f56061528add7a3912ae2714993e4d405060
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-03-01 21:19:49 +0530

    property list made

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,8 @@
+import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
 
 const Home = () => {
@@ -7,6 +10,13 @@ const Home = () => {
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <div className="homeTitle">Browse by property type</div>
+        <PropertyList />
+        <h1 className="homeTitle">Home guests love</h1>
+        <FeaturedProperties />
+      </div>
     </div>
   );
 };
